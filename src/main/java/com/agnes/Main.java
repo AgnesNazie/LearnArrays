92900e05608d06d84dc48a2b9eb256c4f68fb352
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-20 19:50:31 +0100

    more exercises on array

--- src/main/java/com/agnes/Main.java
+++ src/main/java/com/agnes/Main.java
@@ -8,6 +8,10 @@ public class Main {
         exe4();
         exe5();
         exe6();
+        exe7();
+        exe8();
+        exe9();
+        exe10();
     }
 
     public static void exe1() {
@@ -94,19 +98,81 @@ public class Main {
         System.out.print("The oddcount: " + odd);
         System.out.print("\nThe evencount: " + even);
     }
+
     public static void exe6() {
         // calculate the average of array elements[1,2,3,4,5,]
         System.out.println("\nEXERCISE 6");
         //declare and initialize array elements
-         int [] numbers = {1,2,3,4,5};
-         //declare and initialize variable to store data
+        int[] numbers = {1, 2, 3, 4, 5};
+        //declare and initialize variable to store data
         int sum = 0;
         //loop and calculate the sum
-        for (int number :numbers) {
+        for (int number : numbers) {
             sum += number;
         }
         //declare and initialize average
         double average = (double) sum / numbers.length;
         System.out.println("The Average is " + average);
     }
+
+    public static void exe7() {
+        // copy elements from one array to another[5,10,15]
+        // declare and  initialize an array
+        System.out.println("EXERCISE 7");
+        int[] originalNumbers = {5, 10, 15};
+        int[] copiedNumbers = new int[originalNumbers.length];
+        System.arraycopy(originalNumbers, 0, copiedNumbers, 0, originalNumbers.length);
+        for (int copiedNumber : copiedNumbers) {
+            System.out.print(copiedNumber + " ");
+        }
+    }
+
+    public static void exe8() {
+        /* search for a specific number in an array [3,7,9,15]
+         * target [7]
+         */
+        //declare and initialise your array
+        System.out.println("\nEXERCISE 8");
+        int[] numbers = {3, 7, 9, 19};
+        //declare target to search
+        int target = 7;
+        boolean found = false;
+        //loop through the array to search
+        for (int number : numbers) {
+            if (number == target) {
+                found = true;
+                break;
+            }
+        }
+        System.out.println(found ? " Element found " : "Element not found");
+
+    }
+
+    public static void exe9() {
+        int[] numbers = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i += 2) {
+            sum += numbers[i];
+
+        }
+        System.out.println("EXERCISE 9");
+        System.out.println("The sum of even-index is : " + sum);
+    }
+
+    public static void exe10() {
+        // find duplicate elements in an array [1,2,3,1,4,5,2]
+        int[] numbers = {1, 2, 3, 1, 4, 5, 2};
+        System.out.println("EXERCISE 10");
+        System.out.print("Duplicated numbers: ");
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] == numbers[j]) {
+                    System.out.print(numbers[i] + " ");
+                    break;
+                }
+            }
+        }
+
+    }
+
 }
